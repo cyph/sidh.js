3107600e227353218613fa3a70b05abdec419650
Ryan Lester <hacker@linux.com>
2017-04-26 23:41:28 -0400

    type definition fix

--- sidh.d.ts
+++ sidh.d.ts
@@ -1,18 +1,20 @@
-export interface ISIDH {
-	/** Length of shared secret. */
-	bytes: number;
+declare module 'sidh' {
+	interface ISIDH {
+		/** Length of shared secret. */
+		bytes: number;
 
-	/** Private key length. */
-	privateKeyBytes: number;
+		/** Private key length. */
+		privateKeyBytes: number;
 
-	/** Public key length. */
-	publicKeyBytes: number;
+		/** Public key length. */
+		publicKeyBytes: number;
 
-	/** Generates key pair. */
-	keyPair () : {privateKey: Uint8Array; publicKey: Uint8Array};
+		/** Generates key pair. */
+		keyPair () : {privateKey: Uint8Array; publicKey: Uint8Array};
 
-	/** Establishes shared secret. */
-	secret (publicKey: Uint8Array, privateKey: Uint8Array) : Uint8Array;
-};
+		/** Establishes shared secret. */
+		secret (publicKey: Uint8Array, privateKey: Uint8Array) : Uint8Array;
+	}
 
-export const sidh: ISIDH;
+	const sidh: ISIDH;
+}
